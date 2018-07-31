91b140ea7ddd66914955c8225bb21e68571163a7
topias11a705 <topia.s11a7.05+Git@gmail.com>
2018-07-31 20:41:20 +0900

    RecycleViewer installed commit 7/31 13:50

--- app/src/main/java/com/websarva/wings/android/getgaitame/MainActivity.java
+++ app/src/main/java/com/websarva/wings/android/getgaitame/MainActivity.java
@@ -1,13 +1,10 @@
 package com.websarva.wings.android.getgaitame;
 
 import android.content.Context;
-import android.content.res.Resources;
-import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.os.Handler;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v4.view.WindowCompat;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.AsyncTask;
@@ -17,14 +14,10 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AbsListView;
-import android.widget.AdapterView;
 import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -39,24 +32,23 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
-    ArrayList<gaitameDataBox> listData = new ArrayList<>();
-    ImageView imageView;
+    CopyOnWriteArrayList<gaitameDataBox> listData = new CopyOnWriteArrayList<>();
+    CopyOnWriteArrayList<gaitameDataBox> listData_clone = new CopyOnWriteArrayList<>();
+    RecyclerView recycleview; ImageView imageView;LinearLayoutManager mLinearLayoutManager;
     Timer mTimer = new Timer();
     TimerTask mTimerTask = new MainTimerTask();
     Handler mHandler = new Handler();
     boolean menu_flag = true;
     Context contex1;Context contex2;
-    static int position = 0;static int y = 0;
+    int position = 0; int y = 0;
     RecyclerListAdapter adapter=null;
-    RecyclerView recycleview;
-    LinearLayoutManager mLinearLayoutManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -64,7 +56,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         imageView = findViewById(R.id.currency_image_view);
         contex1 = MainActivity.this; contex2 = getApplicationContext();
-
 
         Toolbar toolbar = (Toolbar)findViewById(R.id.tool_bar);
         toolbar.setTitle(R.string.toolbar_title_on);
@@ -82,12 +73,8 @@ public class MainActivity extends AppCompatActivity {
         recycleview.addItemDecoration(new DividerItemDecoration(MainActivity.this, mLinearLayoutManager.getOrientation()));
 
         toolbar.inflateMenu(R.menu.menu);
-        try{
-            mTimer.schedule(mTimerTask, 0, 999);
-        }catch(Exception e){
-            System.out.print(e);
-        }finally {
-        }
+        try{mTimer.schedule(mTimerTask, 0, 999);}
+        catch(Exception e){System.out.print(e);}
         toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem item) {
@@ -133,10 +120,6 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    /*//リストが選択されたときの処理が記述されたメンバクラス。
-    private class ListItemClickListener implements AdapterView.OnItemClickListener {
-        @Override public void onItemClick(AdapterView<?> parent, View view, int position, long id) {}
-    }*/
     //***************************************************************************************************
     private class RecyclerListViewHolder extends RecyclerView.ViewHolder {
         TextView currencyPairCodetextView_textView, bid_textView, ask_textView, open_textView, high_textView, low_textView;
@@ -144,19 +127,19 @@ public class MainActivity extends AppCompatActivity {
 
         public RecyclerListViewHolder(View itemView) {
             super(itemView);
-            currencyPairCodetextView_textView = (TextView) itemView.findViewById(R.id.currencyPairCode);
-            bid_textView = (TextView) itemView.findViewById(R.id.bid);
-            ask_textView = (TextView) itemView.findViewById(R.id.ask);
-            open_textView = (TextView) itemView.findViewById(R.id.open);
-            high_textView = (TextView) itemView.findViewById(R.id.high);
-            low_textView = (TextView) itemView.findViewById(R.id.low);
-            currency_image_view = (ImageView) itemView.findViewById(R.id.currency_image_view);
+            currencyPairCodetextView_textView = (TextView)itemView.findViewById(R.id.currencyPairCode);
+            bid_textView = (TextView)itemView.findViewById(R.id.bid);
+            ask_textView = (TextView)itemView.findViewById(R.id.ask);
+            open_textView = (TextView)itemView.findViewById(R.id.open);
+            high_textView = (TextView)itemView.findViewById(R.id.high);
+            low_textView = (TextView)itemView.findViewById(R.id.low);
+            currency_image_view = (ImageView)itemView.findViewById(R.id.currency_image_view);
         }
     }
     private class RecyclerListAdapter extends RecyclerView.Adapter<RecyclerListViewHolder> {
-         ArrayList<gaitameDataBox> _listData;
+        CopyOnWriteArrayList<gaitameDataBox> _listData;
 
-        public RecyclerListAdapter(ArrayList<gaitameDataBox> listData) {
+        public RecyclerListAdapter(CopyOnWriteArrayList<gaitameDataBox> listData) {
             _listData = listData;
         }
 
@@ -221,13 +204,36 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public synchronized void onPostExecute(String result){
             try{
+                //ArrayList<String> ArraysCopied= new ArrayList<>();
+                CopyOnWriteArrayList<gaitameDataBox> ArraysCopied = new CopyOnWriteArrayList<>();
                 if (adapter != null) {
                     position = mLinearLayoutManager.findFirstVisibleItemPosition();
                     View v = recycleview.getChildAt(0);
                      y = (v == null) ? 0 : (v.getTop() - v.getPaddingTop());
+                    //listData_clone.clear();ArraysCopied.clear();
+
+                    for (gaitameDataBox comB : listData){
+                        ArraysCopied.add(new gaitameDataBox(
+                            new String((String)comB.getCurrencyPairCode()),
+                            new String((String)comB.getOpen()),
+                            new String((String)comB.getHigh()),
+                            new String((String)comB.getLow()),
+                            new String((String)comB.getBid()),
+                            new String((String)comB.getAsk())
+                        ));
+                    }
+
+                    Log .d("JSONObject", ArraysCopied.get(1).getBid());
                     listData.clear();
+                    //TextView text1 = (TextView)(((RecyclerView)findViewById(R.id.lvCityList)).getChildAt(23)).findViewById(R.id.ask);
+
                 }
                 jsonToListData(result);
+                for(int i=0;i<24;i++){
+                    listData.get(i).getBid().equals(ArraysCopied.get(i).getBid());
+                }
+
+
                 if(adapter==null){
                     adapter = new RecyclerListAdapter(listData);
                 }else{
@@ -235,7 +241,7 @@ public class MainActivity extends AppCompatActivity {
                 }
                 recycleview.setAdapter(adapter);
                 mLinearLayoutManager.scrollToPositionWithOffset(position, y);
-            }catch(Exception ex) {System.out.print(ex);}
+            }catch(Exception ex) {ex.printStackTrace();}
         }
     private synchronized void jsonToListData(String result) throws JSONException{
         JSONObject rootJSON = new JSONObject(result);
